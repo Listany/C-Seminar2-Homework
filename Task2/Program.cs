357e32d6f4b5166246ace5bc44cb6ae29711f689
Listratova Tatyana <listratova1@gmail.com>
2022-09-07 21:11:41 +0300

    added Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿System.Console.WriteLine("Введите число: ");
+int num = 0;
+
+try
+{
+    num = int.Parse(Console.ReadLine());
+
+    int numModul = Math.Abs(num);
+
+    if (numModul<100)
+    {
+        System.Console.WriteLine("В введенном числе отсутствует третья цифра");
+    }
+    else
+    {
+        string numStr = Convert.ToString(numModul);
+
+        System.Console.WriteLine(numStr[2]);
+
+    }
+}
+catch
+{
+    System.Console.WriteLine("Вы ввели некорректные данные");
+}
+
+ 
+
+
+
+
+
+
