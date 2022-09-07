862f8683af250ee7c98be88262568f5be214e517
Listratova Tatyana <listratova1@gmail.com>
2022-09-07 21:20:40 +0300

    added Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿int numWeek=0;
+
+try
+{
+    System.Console.WriteLine("Введите день недели - от 1 до 7");
+
+    numWeek=int.Parse(Console.ReadLine());
+
+    switch(numWeek)
+    {
+        case 6:
+        case 7:
+            System.Console.WriteLine("Сегодня выходной!");
+            break;
+        default:
+            System.Console.WriteLine("Сегодня будний день...");
+            break;
+    }
+}
+catch
+{
+    System.Console.WriteLine("Вы ввели некорректные данные");
+}
