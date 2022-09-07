4db76c47e87325e6a3803a13c71aec20d7ae2108
Listratova Tatyana <listratova1@gmail.com>
2022-09-07 19:55:21 +0300

    added Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿System.Console.WriteLine("Введите трехзначное число: ");
+
+int num=0;
+
+try
+{
+    num = int.Parse(Console.ReadLine());
+    
+
+    if (num<100 | num>999)
+    {
+        System.Console.WriteLine("Вы ввели не трехзначное число, попробуйте снова");
+    }
+    else
+    {
+        int result = num / 10 % 10;
+        System.Console.WriteLine($"Вторая цифра заданного числа = {result}");
+    }
+}
+catch
+{
+    System.Console.WriteLine("Вы ввели недопустимое значение!");
+}
+
